d717cf92e4acfb54529705266284cc8b4e5ea922
Lukang11 <77103986+Lukang11@users.noreply.github.com>
2021-03-31 17:02:46 +0200

    Create 3.1d.php

---
+++ 2/3.1d.php
@@ -0,0 +1,26 @@
+<?php
+$tab[0]=rand(0,30);
+$tab[1]=rand(0,30);
+$tab[2]=rand(0,30);
+$tab[3]=rand(0,30);
+$tab[4]=rand(0,30);
+$tab[5]=rand(0,30);
+$tab[6]=rand(0,30);
+echo ("Twoja tablica :");
+$z = $tab[0];
+foreach($tab as $wartosc)
+{
+   echo(" $tab[$i], ");
+
+   if($tab[$i] >= $tab[$i + 1] && $tab[$i] > $z)
+    {
+        $z = $tab[$i];
+
+    }
+    else if ($tab[$i] <= $tab[$i + 1] && $tab[$i] > $z){
+        $z = $tab[$i + 1];
+    }
+}
+echo("Najwiekszy elemnt tablicy to :$z");
+
+?>
