dce82d2bee24b21e8c26d0ab79a691222e3b8a91
Lukang11 <77103986+Lukang11@users.noreply.github.com>
2021-03-31 14:37:40 +0200

    Create 2.2.php

---
+++ 2/2.2.php
@@ -0,0 +1,10 @@
+<?php
+$tablica["niemcy"] = "Niemiecka";
+$tablica["polska"] ="Polska";
+$tablica["hiszpania"] ="Hiszpańska";
+$tablica["holandia"] ="Holenderska";
+$tablica["anglia"] ="Angielska";
+$tablica["włochy"] ="Włoska";
+$z = "niemcy";
+echo ("Twoja nardowość to narodowość $tablica[$z].");
+?>
